1da0698e231d5710d50b1ff3398d9aa6ab8465d8
Narek <157054887+nkarapet42@users.noreply.github.com>
2024-10-10 00:03:07 +0400

    Add files via upload

---
+++ IntershipC#/IntershipC#/Math.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Numerics;
+
+namespace MyArea
+{
+    class Circle
+    {
+        private double Radius { get; }
+
+        public Circle(double radius) => Radius = radius;
+
+        public static double AreaOfCircle(Circle circle)
+        {
+            if (circle == null) throw new Exception("Create Circle");
+            if (circle.Radius <= 0) throw new Exception("Invalid radius");
+
+            return (Math.PI * circle.Radius * circle.Radius);
+        }
+    }
+
+    class Triangle
+    {
+        private double A { get; }
+        private double B { get; }
+        private double C { get; }
+
+        private static void  CheckTriangle (Triangle point)
+        {
+            if (point == null)
+                throw new Exception("Create the triangle");
+            if (point.A <= 0 || point.B <= 0 || point.C <= 0)
+                throw new Exception("Triangle sides is not valid");
+            if (point.A + point.B <= point.C
+                || point.A + point.C <= point.B
+                || point.B + point.C <= point.A)
+                throw new Exception("Triangle is wrong");
+        }
+
+        public  static void CheckRightTriangle(Triangle point)
+        {
+            CheckTriangle(point);
+            double a = Math.Pow(point.A, 2);
+            double b = Math.Pow(point.B, 2);
+            double c = Math.Pow(point.C, 2);
+            if ((a + b == c) || (a + c == b) || (c + b == a))
+                Console.WriteLine("Triangle is Right");
+            else Console.WriteLine("Triangle isn't Right");
+        }
+
+        public static double AreaOfTriangle (Triangle point)
+        {
+            CheckTriangle(point);
+            double s = (point.A + point.B + point.C) / 2;
+            double area = Math.Sqrt(s * (s - point.A) * (s - point.B) * (s - point.C));
+            return (area);
+        }
+
+        public Triangle(double a, double b, double c) => (A, B, C) = (a, b, c);
+    }
+}
---
+++ IntershipC#/IntershipC#/Program.cs
@@ -0,0 +1,135 @@
+﻿using MyArea;
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+
+namespace UnitTest
+{
+    class Program
+    {
+        public static void Reaload()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Main();
+        }
+
+        public static void Main()
+        {
+            Console.Write("Choose the mode (1-> Enter your numbers, 2-> UnitTest, 3-> Exit): ");
+            string tmp = Console.ReadLine();
+            try
+            {
+                int mode = int.Parse(tmp);
+                if (mode == 2)
+                {
+                    Console.WriteLine("*" + new string('#', 45) + "*");
+                    try
+                    {
+                        Triangle point = new Triangle(4, 3, 5);
+                        Triangle.CheckRightTriangle(point);
+                    }
+                    catch (Exception e) { Console.WriteLine(e.Message); }
+                    try
+                    {
+                        Triangle point = new Triangle(45, 57, 27);
+                        Console.WriteLine("Area of Triangle: " + Triangle.AreaOfTriangle(point));
+                        Triangle.CheckRightTriangle(point);
+                    }
+                    catch (Exception e) { Console.WriteLine(e.Message); }
+                    try
+                    {
+                        Console.WriteLine(Triangle.AreaOfTriangle(null));
+                    }
+                    catch (Exception e) { Console.WriteLine(e.Message); }
+                    try
+                    {
+                        Console.WriteLine(Triangle.AreaOfTriangle(new Triangle(45, 57, 0)));
+                    }
+                    catch (Exception e) { Console.WriteLine(e.Message); }
+                    try
+                    {
+                        Console.WriteLine(Triangle.AreaOfTriangle(new Triangle(45, 57, 786)));
+                    }
+                    catch (Exception e) { Console.WriteLine(e.Message); }
+                    Console.WriteLine("*" + new string('#', 45) + "*");
+                    Console.Write(new string('\n', 3));
+                    Console.WriteLine("*" + new string('#', 45) + "*");
+                    try
+                    {
+                        Console.WriteLine("Area of Circle: " + Circle.AreaOfCircle(new Circle(4)));
+                    }
+                    catch (Exception e) { Console.WriteLine(e.Message); }
+                    try
+                    {
+                        Console.WriteLine(Circle.AreaOfCircle(new Circle(-5)));
+                    }
+                    catch (Exception e) { Console.WriteLine(e.Message); }
+                    try
+                    {
+                        Console.WriteLine(Circle.AreaOfCircle(null));
+                    }
+                    catch (Exception e) { Console.WriteLine(e.Message); }
+                    Console.WriteLine("*" + new string('#', 45) + "*");
+                    Reaload();
+                }
+                else if (mode == 1)
+                {
+                    string status = "";
+                    while (status.ToUpper() != "EXIT")
+                    {
+                        Console.WriteLine("Command's for clear (CLEAN) or exit (EXIT)");
+                        Console.Write("Create treangle (1) or circle (2): Enter the number : ");
+                        string temp = Console.ReadLine();
+                        if (temp == "1")
+                        {
+                            try
+                            {
+                                Console.Write("Enter side A: ");
+                                double sideA = double.Parse(Console.ReadLine());
+                                Console.Write("Enter side B: ");
+                                double sideB = double.Parse(Console.ReadLine());
+                                Console.Write("Enter side C: ");
+                                double sideC = double.Parse(Console.ReadLine());
+
+                                Triangle triangle = new Triangle(sideA, sideB, sideC);
+                                Console.WriteLine("Area of Triangle: " + Triangle.AreaOfTriangle(triangle));
+                                Triangle.CheckRightTriangle(triangle);
+                            }
+                            catch (Exception e) { Console.WriteLine(e.Message + ", press any key to continue"); Console.ReadKey(); Console.Clear(); }
+                        }
+                        else if (temp == "2")
+                        {
+                            try
+                            {
+                                Console.Write("Enter radius: ");
+                                double radius = double.Parse(Console.ReadLine());
+
+                                Circle circle = new Circle(radius);
+                                Console.WriteLine($"Area of Circle {Circle.AreaOfCircle(circle)}");
+                            }
+                            catch (Exception e) { Console.WriteLine(e.Message + ", press any key to continue"); Console.ReadKey(); Console.Clear(); }
+                        }
+                        else if (temp.ToUpper() == "CLEAN")
+                            Console.Clear();
+                        else if (temp.ToUpper() != "EXIT")
+                            Console.WriteLine("Invalid given command, please try again");
+                        status = temp;
+                    }
+                }
+                else if (mode == 3)
+                    return;
+                else
+                {
+                    Console.WriteLine("Invalid given command, press any key to try again");
+                    Reaload();
+                }
+            } 
+            catch (Exception e) 
+            { 
+                Console.WriteLine($"Invalid command {tmp} , press any key to try again");
+                Reaload();
+            }
+        }
+    }
+}
